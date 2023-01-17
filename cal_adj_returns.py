3e35e2c6f7702ffe23230d6cddc0a04bff0123ae
imahdimir <imahdimir@gmail.com>
2023-01-17 17:14:15 +0330

    create

---
+++ cal_adj_returns.py
@@ -0,0 +1,40 @@
+"""
+
+    """
+
+from pathlib import Path
+
+import pandas as pd
+from githubdata import GitHubDataRepo
+from mirutil.ns import update_ns_module
+from persiantools.jdatetime import JalaliDateTime
+
+update_ns_module()
+import ns
+
+gdu = ns.GDU()
+c = ns.Col()
+
+sfp = Path('temp.prq')
+
+class ColName :
+    pass
+
+cn = ColName()
+
+def main() :
+    pass
+
+    ##
+
+
+
+    ##
+
+
+##
+
+
+if __name__ == "__main__" :
+    main()
+    print(f'{Path(__file__).name} Done!')
